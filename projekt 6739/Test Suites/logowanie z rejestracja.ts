e6aede742c503f81c0c929f446e3ef859536a978
ReedXE <patr.siwak@gmail.com>
2021-01-14 15:14:42 +0100

    1.2

---
+++ projekt 6739/Test Suites/logowanie z rejestracja.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>logowanie z rejestracja</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6b99811c-c5ae-4b14-9383-79e3ca3e3600</testSuiteGuid>
+   <testCaseLink>
+      <guid>7c953626-4cb0-4575-a847-3d7ab93b690a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/logowanie na stronie/logowanie i rejestracja</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
