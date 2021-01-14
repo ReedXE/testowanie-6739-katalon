58af0fb4184015ce0bb03af9e3cd9436255f1d96
ReedXE <patr.siwak@gmail.com>
2021-01-14 14:15:12 +0100

    1.0

---
+++ projekt 6739/Test Suites/logowanie na stronie.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>logowanie na stronie</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>41af1966-bb9d-48c2-9dbe-f3e0d786bcbe</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcc1851b-7c4f-454a-8d8a-571abb0ce68b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/logowanie na stronie/logowanie prawidlowe</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3331aa70-eb4d-4ab3-839b-d0c50dc31a24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/logowanie na stronie/logowanie bledne</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
